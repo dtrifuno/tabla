a83cac8563534288433fd3918daa93495322e5bb
Darko Trifunovski <dtrifuno@gmail.com>
2019-03-19 22:40:11 -0500

    z

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,17 +7,34 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
+:root {
+  --red: #e6261f;
+  --orange: #eb7532;
+  --yellow: #f7d038;
+  --limegreen: #a3e048;
+  --shamrock: #49da9a;
+  --azure: #34bbe6;
+  --royalblue: #4355db;
+  --violet: #d23be7;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.item-dropzone-area {
+  background-color: green;
+  width: 100%;
+  height: 1rem;
 }
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
 
-Vue.use(Router)
+import Router from "vue-router";
+
+import HelloWorld from "@/components/HelloWorld";
+import TableView from "@/components/TableView";
+
+//import "vue-material-design-icons/styles.css";
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "TableView",
+      component: TableView
+    },
+    {
+      path: "/table",
+      name: "HelloWorld",
       component: HelloWorld
     }
   ]
-})
+});
